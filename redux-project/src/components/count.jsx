cbb1afd5283e2384ac68f2cadf4a310262436334
shixiaoxueya <email@example.comemail@example.com>
2020-02-11 13:43:40 +0800

    提交redux第一个练习

---
+++ redux-project/src/components/count.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+    //原状态
+    state = {
+        number : 0
+    }
+
+    //加法
+    increment =()=>{
+        //获取用户选择的数字
+        const value = this.refs.checkNumber.value
+        //更新状态
+        const number = this.state.number
+        this.setState({number:number + value*1})
+    }
+     //减法
+     decrement =()=>{
+        //获取用户选择的数字
+        const value = this.refs.checkNumber.value
+        //更新状态
+        const number = this.state.number
+        this.setState({number:number - value*1})
+    }
+    //奇数才加
+    incrementOdd =()=>{
+        //获取用户选择的数字
+        const value = this.refs.checkNumber.value
+        //更新状态
+        const number = this.state.number
+        if(number%2 ===1 ){
+            this.setState({number:number + value*1})
+        }
+    }
+    //延迟加
+    incrementAsync =()=>{
+        //获取用户选择的数字
+        const value = this.refs.checkNumber.value
+        //更新状态
+        const number = this.state.number
+        setTimeout(() => {
+            this.setState({number:number + value*1})
+        }, 1000);
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>当前计数为:{this.state.number}</h2>
+                <select ref="checkNumber">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+
+                <button onClick={this.increment}> + </button>&nbsp;
+                <button onClick={this.decrement}> - </button>&nbsp;
+                <button onClick={this.incrementOdd}> 奇数相加 </button>&nbsp;
+                <button onClick={this.incrementAsync}> 延迟相加 </button>&nbsp;
+            </div>
+        )
+    }
+}
